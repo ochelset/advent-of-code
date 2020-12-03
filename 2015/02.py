f7f0adf95f2ba8b39e3968be7a8755e0baa5e229
Ole Christian Helset <ochelset@gmail.com>
2020-12-03 08:42:09 +0100

    Started on the 2015 calendar too

---
+++ 2015/02.py
@@ -0,0 +1,76 @@
+"""
+--- Day 2: I Was Told There Would Be No Math ---
+The elves are running low on wrapping paper, and so they need to submit an order for more. They have a list of the dimensions (length l, width w, and height h) of each present, and only want to order exactly as much as they need.
+
+Fortunately, every present is a box (a perfect right rectangular prism), which makes calculating the required wrapping paper for each gift a little easier: find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l. The elves also need a little extra paper for each present: the area of the smallest side.
+
+For example:
+
+A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
+A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
+
+All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
+
+Your puzzle answer was 1586300.
+
+The first half of this puzzle is complete! It provides one gold star: *
+
+--- Part Two ---
+The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the length they need to order, which they would again like to be exact.
+
+The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
+
+For example:
+
+A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+
+How many total feet of ribbon should they order?
+
+Your puzzle answer was 3737498.
+"""
+
+import math
+
+presents = open("data/02.data").read().strip().split("\n")
+testdata = """
+3x4x2
+2x3x4
+1x1x10
+"""
+
+def calculateWrappingPaperFor(present: str) -> int:
+    l, w, h = [int(n) for n in present.split("x")]
+    a, b, c = (l*w, w*h, h*l)
+    return 2*a + 2*b + 2*c + min(a, b, c)
+
+def calculateRibbonAndBowFor(present: str) -> int:
+    dimensions = sorted([int(n) for n in present.split("x")])
+    ribbon = 2 * dimensions[0] + 2 * dimensions[1]
+    bow = math.prod(dimensions)
+    return ribbon + bow
+
+def part1(presents: list) -> int:
+    total = 0
+    for present in presents:
+        total += calculateWrappingPaperFor(present)
+
+    return total
+
+def part2(presents: list) -> int:
+    total = 0
+    for present in presents:
+        total += calculateRibbonAndBowFor(present)
+
+    return total
+
+#for present in testdata.strip().split("\n"):
+#    paper = calculateWrappingPaperFor(present)
+#    ribbon = calculateRibbonAndBowFor(present)
+#    print(present, ">", paper, ribbon)
+
+result1 = part1(presents)
+result2 = part2(presents)
+
+print("Part 1: The elves must order", result1, "square feet of wrapping paper")
+print("Part 2: They also need", result2, "feet of ribbon")
